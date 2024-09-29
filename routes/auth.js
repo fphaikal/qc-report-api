6710b5768f212b2ce714a84a8bc3a10d62be96a2
Fahreza Pasha Haikal <abufahreza@gmail.com>
2024-09-30 00:33:06 +0700

    minor update
    - change name for type_ng
    - add user profile route

--- routes/auth.js
+++ routes/auth.js
@@ -4,8 +4,11 @@ const bcrypt = require("bcrypt");
 
 const db = require("../lib/db");
 const Auth = require("../lib/auth");
+const UserProfile = require("../lib/auth/userProfile");
 
 router.post("/login", Auth.login);
 router.post("/register", Auth.register);
+router.get("/user", UserProfile.get);
+router.put("/user", UserProfile.update);
 
 module.exports = router;
