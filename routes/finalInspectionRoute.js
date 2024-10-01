0e39f59d5796cbb1996dbf014731db7acdf546e0
Fahreza Pasha Haikal <abufahreza@gmail.com>
2024-10-01 16:37:21 +0700

    minor update

--- routes/finalInspectionRoute.js
+++ routes/finalInspectionRoute.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const FinalInspection = require('../lib/report/finalInspection');
 
 router.get('/', FinalInspection.get);
+router.get('/operator', FinalInspection.getOperator);
+router.get('/chartData', FinalInspection.chartData);
 router.post('/', FinalInspection.create);
 router.put('/', FinalInspection.update);
 router.delete('/:id', FinalInspection.delete);
