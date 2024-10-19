ec34189716824b9b72899a82b652b78728f77510
Fahreza Pasha Haikal <abufahreza@gmail.com>
2024-10-19 21:38:14 +0700

    update cron job

--- cron/index.js
+++ cron/index.js
@@ -1,14 +1,25 @@
 const cron = require('node-cron');
-const db = require("../lib/db");
+const db = require("../lib/db"); // Pastikan db menggunakan pool koneksi
 
 // Menghapus token dari database setiap hari pada jam 3 pagi
 cron.schedule('0 3 * * *', () => {
   const sql = `DELETE FROM users WHERE token < NOW()`; // Contoh, sesuaikan dengan schema database Anda
-  db.query(sql, (err, result) => {
+
+  db.getConnection((err, connection) => {
     if (err) {
-      console.error("Error deleting expired tokens:", err);
-    } else {
-      console.log("Expired tokens deleted");
+      console.error("Error getting database connection:", err);
+      return;
     }
+    
+    connection.query(sql, (err, result) => {
+      if (err) {
+        console.error("Error deleting expired tokens:", err);
+      } else {
+        console.log("Expired tokens deleted");
+      }
+
+      // Release the connection back to the pool
+      connection.release();
+    });
   });
 });
